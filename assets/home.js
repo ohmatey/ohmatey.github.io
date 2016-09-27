72ce1038eb9bf48f289d838f6533efb90361e67f
Aaron McPherson <aaronmcpherson@hotmail.com>
2016-09-27 21:47:05 +0700

    fix initial resize of hero

--- assets/home.js
+++ assets/home.js
@@ -3,10 +3,15 @@ var homeCarousel = new Carousel();
 homeCarousel.init();
 
 (function(){
-    (window.onresize = function(){
+    function resizeHero(){
         var $hero = document.getElementById('intro-inner');
         var $mask = document.getElementById('mask');
         var maskH = window.getComputedStyle($mask, null).getPropertyValue('height')
         $hero.style.padding = maskH + " 0 0";
-    })();
+    }
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        (window.onresize = resizeHero())
+    });
 })();
