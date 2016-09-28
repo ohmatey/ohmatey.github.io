a9d31de5523ca74f81a12ceb4a75af5d5b5fd210
Aaron McPherson <aaronmcpherson@hotmail.com>
2016-09-28 10:32:59 +0700

    fix intro

--- assets/home.js
+++ assets/home.js
@@ -4,14 +4,19 @@ homeCarousel.init();
 
 (function(){
     function resizeHero(){
+        console.log("asd")
         var $hero = document.getElementById('intro-inner');
         var $mask = document.getElementById('mask');
         var maskH = window.getComputedStyle($mask, null).getPropertyValue('height')
+        
+        if(maskH === 0){
+            maskH = "100%";
+        }
+
         $hero.style.padding = maskH + " 0 0";
     }
+    resizeHero();
 
 
-    document.addEventListener("DOMContentLoaded", function() {
-        (window.onresize = resizeHero())
-    });
+    window.addEventListener('resize', resizeHero, false);
 })();
